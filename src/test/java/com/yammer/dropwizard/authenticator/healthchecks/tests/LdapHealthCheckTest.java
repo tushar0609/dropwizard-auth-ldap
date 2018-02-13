fefa3fbd2ab8f5eb6d02544727ed5f71b80f2333
phaneesh <phaneesh.n@gmail.com>
2018-02-13 11:08:43 +0530

    DW Upgrade

--- src/test/java/com/yammer/dropwizard/authenticator/healthchecks/tests/LdapHealthCheckTest.java
+++ src/test/java/com/yammer/dropwizard/authenticator/healthchecks/tests/LdapHealthCheckTest.java
@@ -1,6 +1,5 @@
 package com.yammer.dropwizard.authenticator.healthchecks.tests;
 
-import com.codahale.metrics.health.HealthCheck;
 import com.yammer.dropwizard.authenticator.LdapAuthenticator;
 import com.yammer.dropwizard.authenticator.LdapCanAuthenticate;
 import com.yammer.dropwizard.authenticator.LdapConfiguration;
@@ -9,9 +8,8 @@ import com.yammer.dropwizard.authenticator.healthchecks.LdapHealthCheck;
 import io.dropwizard.auth.basic.BasicCredentials;
 import org.junit.Test;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.core.IsNot.not;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -23,13 +21,13 @@ public class LdapHealthCheckTest {
         LdapAuthenticator ldapAuthenticator = mock(LdapAuthenticator.class);
         when(ldapAuthenticator.authenticate(any(BasicCredentials.class))).thenReturn(true);
         LdapHealthCheck healthCheck = new LdapHealthCheck<>(new ResourceAuthenticator(ldapAuthenticator));
-        assertThat(healthCheck.check(), is(HealthCheck.Result.healthy()));
+        assertTrue(healthCheck.check().isHealthy());
     }
 
     @Test
     public void unhealthy() throws Exception {
         final LdapAuthenticator badLdapAuthenticator = new LdapCanAuthenticate(new LdapConfiguration());
         final LdapHealthCheck badHealthCheck = new LdapHealthCheck<>(new ResourceAuthenticator(badLdapAuthenticator));
-        assertThat(badHealthCheck.check(), not(HealthCheck.Result.healthy()));
+        assertFalse(badHealthCheck.check().isHealthy());
     }
 }
